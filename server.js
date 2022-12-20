818c932198bd209f237096c2b473612ddf945594
CodeSevenic <sevenicbookings@gmail.com>
2022-12-20 09:50:09 +0200

    SEO Blog Code Update

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const morgan = require('morgan');
-const express = require('express');
-const express = require('express');
-const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+// app
+const app = express();
+
+// middleware
+app.use(morgan('dev'));
+// body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(cookieParser());
+
+// cors
+app.use(cors());
