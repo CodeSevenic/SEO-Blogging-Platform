3b172a237820d379ae6aff076c1e71ce39e3392d
CodeSevenic <sevenicbookings@gmail.com>
2022-12-25 10:27:47 +0200

    SEO Blog Code Update

--- models/user.js
+++ models/user.js
@@ -1,55 +1,65 @@
 ﻿const mongoose = require('mongoose');
 const crypto = require('crypto');
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    trim: true,
-    required: true,
-    maxLength: 32,
-    unique: true,
-    index: true,
-    lowerCase: true,
-  },
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      required: true,
+      maxLength: 32,
+      unique: true,
+      index: true,
+      lowerCase: true,
+    },
 
-  name: {
-    type: String,
-    trim: true,
-    required: true,
-    maxLength: 32,
-  },
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      maxLength: 32,
+    },
 
-  email: {
-    type: String,
-    trim: true,
-    required: true,
-    lowerCase: true,
-    unique: true,
-  },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      lowerCase: true,
+      unique: true,
+    },
 
-  profile: {
-    type: String,
-    required: true,
-  },
+    profile: {
+      type: String,
+      required: true,
+    },
 
-  hashed_password: {
-    type: String,
-    required: true,
-  },
+    hashed_password: {
+      type: String,
+      required: true,
+    },
 
-  salt: String,
+    salt: String,
 
-  about: {
-    type: String,
-  },
+    about: {
+      type: String,
+    },
 
-  role: {
-    type: Number,
-    trim: true,
-  },
+    role: {
+      type: Number,
+      trim: true,
+    },
 
-  photo: {
-    data: Buffer,
-    contentType: String,
+    photo: {
+      data: Buffer,
+      contentType: String,
+    },
+
+    resetPasswordLink: {
+      data: String,
+      default: '',
+    },
   },
-});
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('User', userSchema);
