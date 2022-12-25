251eab3a9b372336586ef4fe9f01f0e4067881cc
CodeSevenic <sevenicbookings@gmail.com>
2022-12-25 10:10:55 +0200

    SEO Blog Code Update

--- models/user.js
+++ models/user.js
@@ -1,1 +1,14 @@
-﻿
+﻿const mongoose = require('mongoose');
+const crypto = require('crypto');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    trim: true,
+    required: true,
+    maxLength: 32,
+    unique: true,
+    index: true,
+    lowerCase: true,
+  },
+});
