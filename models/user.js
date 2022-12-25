d8dcadf87f66d6c693ae3aeff25ecbab9545a5d1
CodeSevenic <sevenicbookings@gmail.com>
2022-12-25 10:21:16 +0200

    SEO Blog Code Update

--- models/user.js
+++ models/user.js
@@ -11,12 +11,14 @@ const userSchema = new mongoose.Schema({
     index: true,
     lowerCase: true,
   },
+
   name: {
     type: String,
     trim: true,
     required: true,
     maxLength: 32,
   },
+
   email: {
     type: String,
     trim: true,
@@ -24,14 +26,30 @@ const userSchema = new mongoose.Schema({
     lowerCase: true,
     unique: true,
   },
+
   profile: {
     type: String,
     required: true,
   },
+
   hashed_password: {
     type: String,
     required: true,
   },
+
   salt: String,
-  about: {},
+
+  about: {
+    type: String,
+  },
+
+  role: {
+    type: Number,
+    trim: true,
+  },
+
+  photo: {
+    data: Buffer,
+    contentType: String,
+  },
 });
