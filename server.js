23ca07cdd802a3a47c9f0b8094ef2b6a129f6fa2
CodeSevenic <sevenicbookings@gmail.com>
2022-12-25 09:31:53 +0200

    SEO Blog Code Update

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 
 // db
 mongoose
-  .connect(process.env.DATABASE, {
+  .connect(process.env.DATABASE_CLOUD, {
     useNewUrlParser: true,
     // useCreateIndex: true,
     // useFindAndModify: false,
