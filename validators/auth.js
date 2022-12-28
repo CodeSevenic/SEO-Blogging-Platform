e0aa787ffadcf29e3416080950a3a79cba8e1bca
CodeSevenic <sevenicbookings@gmail.com>
2022-12-28 16:26:57 +0200

    SEO Blog Code Update

--- validators/auth.js
+++ validators/auth.js
@@ -5,3 +5,7 @@ exports.userSignupValidator = [
   check('email').isEmail().withMessage('Please enter valid email address'),
   check('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
 ];
+exports.userSignInValidator = [
+  check('email').isEmail().withMessage('Please enter valid email address'),
+  check('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
+];
