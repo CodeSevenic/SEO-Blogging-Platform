b7a7a36c98861a16f4364caa282959c4f2fffe51
CodeSevenic <sevenicbookings@gmail.com>
2022-12-28 16:02:59 +0200

    SEO Blog Code Update

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,7 @@
-﻿exports.signup = (req, res) => {
+﻿const User = require('../models/user');
+const shortId = require('shortid');
+
+exports.signup = (req, res) => {
   const { name, email, password } = req.body;
   res.json({
     user: { name, email, password },
